b49ad6ce736b495e5a79a17c43e233e31bb0eaca
Loafus <eth.fish@gmail.com>
2018-09-16 18:32:51 -0700

    pause/play working

--- javascript/main.js
+++ javascript/main.js
@@ -14,7 +14,7 @@ function addButton(topic) {
     $btns.append(newBtn)
 }
 
-for (i=0; i<topics.length; i++) {
+for (i = 0; i < topics.length; i++) {
     addButton(topics[i])
 }
 
@@ -30,7 +30,7 @@ $('#submit').on('click', function (event) {
 $(document).on('click', '.topic-btn', function () {
 
     var queryUrl = 'https://api.giphy.com/v1/gifs/search?q=' + $(this).text() + '&api_key=krvOHtQ3ZxpkyWOvzQxM1d2GyBEcCra1&limit=10'
-    
+
     $.ajax({
         url: queryUrl,
         method: 'GET'
@@ -46,10 +46,34 @@ $(document).on('click', '.topic-btn', function () {
             var p = $("<p>").text("Rating: " + rating)
             var gif = $("<img>")
 
-            gif.attr("src", re[i].images.fixed_height.url)
+            gif.attr(
+                "src", re[i].images.fixed_height_still.url
+            ).attr(
+                "data-still", re[i].images.fixed_height_still.url
+            ).attr(
+                "data-animate", re[i].images.fixed_height.url
+            ).attr(
+                "data-state", "still"
+            ).attr(
+                "class", "gif"
+            )
+
             gifDiv.append(p)
             gifDiv.append(gif)
             $gifs.prepend(gifDiv)
         }
     })
+})
+
+$(document).on('click', '.gif', function () {
+    
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
 })
